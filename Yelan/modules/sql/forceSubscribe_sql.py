ba4019160a7322d6ca32822b32f1f91f2fd997c9
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-07 19:32:28 +0530

    Update forceSubscribe_sql.py

--- Yelan/modules/sql/forceSubscribe_sql.py
+++ Yelan/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from YorForger.modules.sql import BASE, SESSION
+from Yelan.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
