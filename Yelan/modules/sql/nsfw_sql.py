5901b5690bfa09462c17e753330e20bb78e92dca
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-07 19:35:27 +0530

    Update nsfw_sql.py

--- Yelan/modules/sql/nsfw_sql.py
+++ Yelan/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from YorForger.modules.sql import BASE, SESSION
+from Yelan.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
 #   |        Kang with Credits         |
