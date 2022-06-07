9f16af28f73a90d8bd76e05f15916dfb58cd934f
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-07 19:12:20 +0530

    Update __init__.py

--- Yelan/modules/sql/__init__.py
+++ Yelan/modules/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from YorForger import DB_URI, LOGGER
+from Yelan import DB_URI, LOGGER
 
 
 def start() -> scoped_session:
