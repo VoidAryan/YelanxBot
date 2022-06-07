f93b299c7a2efb8bb5969460f9d8a80f95bc4092
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-07 19:35:59 +0530

    Update reporting_sql.py

--- Yelan/modules/sql/reporting_sql.py
+++ Yelan/modules/sql/reporting_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean
 from sqlalchemy.sql.sqltypes import BigInteger
-from YorForger.modules.sql import SESSION, BASE
+from Yelan.modules.sql import SESSION, BASE
 
 
 class ReportingUserSettings(BASE):
