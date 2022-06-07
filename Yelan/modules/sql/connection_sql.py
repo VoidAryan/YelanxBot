da9e2984b3d8c4ce203657baa11c83576c6c1f93
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-07 19:16:11 +0530

    Update connection_sql.py

--- Yelan/modules/sql/connection_sql.py
+++ Yelan/modules/sql/connection_sql.py
@@ -5,7 +5,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
-from YorForger.modules.sql import SESSION, BASE
+from Yelan.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
