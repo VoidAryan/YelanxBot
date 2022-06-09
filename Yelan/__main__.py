18ae4f2ca6b390bd2d23af8271a4cc57149bb771
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-06-09 13:04:30 +0530

    Update __main__.py

--- Yelan/__main__.py
+++ Yelan/__main__.py
@@ -214,7 +214,7 @@ def test(update: Update, context: CallbackContext):
     print(update.effective_message)
 
 
-def start(update: Update, context: CallbackContext, update: ParseMode):
+def start(update: Update, context: CallbackContext, ParseMode):
     args = context.args
     uptime = get_readable_time((time.time() - StartTime))
     if update.effective_chat.type == "private":
